858565b593d425ab8a47a1b245cda20e678381af
Seokisun <42824911+Seokisun@users.noreply.github.com>
2018-09-17 22:14:36 +0900

    Add files via upload

---
+++ recbinsearch.py
@@ -0,0 +1,28 @@
+import time
+import random
+
+#l = low / u = upper / L = List /  target = key
+
+def recbinsearch(L, l, u, target):
+    middle = (l + y) //2
+    if middle = target:
+        return middle
+    elif middle > target:
+        
+
+    elif middle < target:
+
+
+
+numofnbrs = int(input("Enter a number: "))
+numbers = []
+for i in range(numofnbrs):
+    numbers += [random.randint(0, 999999)]
+
+numbers = sorted(numbers)
+
+numoftargets = int(input("Enter the number of targets: "))
+targets = []
+for i in range(numoftargets):
+    targets += [random.randint(0, 999999)]
+
